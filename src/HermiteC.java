cd480154bf786cbf710ff9b0d1a656f975fb2782
aniketranag123@gmail.com <aniketranag123@gmail.com>
2021-03-15 10:20:47 +0530

    Computer Graphics

--- src/HermiteC.java
+++ src/HermiteC.java
@@ -11,16 +11,16 @@ public class HermiteC {
             public void paint(Graphics graphics)
             {
 
-                for(float u=0;u<=1;u+=0.001)
+                for(float u=0.0f;u<=1.0;u+=0.001)
                 {
                     int temp_x= (int)((2*u*u*u-3*u*u+1)*x[0]+(-2*u*u*u+3*u*u)*x[1]+(u*u*u-2*u*u+u)*tangent[0]+(u*u*u-u*u)*tangent[1]);
-                    int temp_y=(int)((2*u*u*u-3*u*u+1)*y[0]+(-2*u*u*u-3*u*u)*y[1]+(u*u*u-2*u*u+u)*tangent[0]+(u*u*u-u*u)*tangent[1]);
-                    graphics.drawLine(temp_x,temp_y,temp_x,temp_y);
+                    int temp_y=(int)((2*u*u*u-3*u*u+1)*y[0]+(-2*u*u*u+3*u*u)*y[1]+(u*u*u-2*u*u+u)*tangent[0]+(u*u*u-u*u)*tangent[1]);
+                    graphics.drawOval(temp_x,temp_y,2,2);
                 }
             }
         }
 
-        JFrame jFrame = new JFrame("Bezier Curve");
+        JFrame jFrame = new JFrame("Hermite Curve");
         jFrame.setSize(1000, 1000);
         jFrame.setResizable(false);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
